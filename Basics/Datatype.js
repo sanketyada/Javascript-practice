bfa96b518d9fd4b30361db3f2a5cea541ac0e179
Sanket Yadav <160760290+sanketyada@users.noreply.github.com>
2025-02-03 12:24:23 +0000

    datatypes

---
+++ Basics/Datatype.js
@@ -0,0 +1,19 @@
+let name="Sanket Yadav"
+let age=20;
+let marks=90.34
+let passed=true;
+let temprature=null;
+let caste;
+ 
+console.log(typeof caste)
+console.log(name);
+
+/*
+number
+boolean
+String  
+null
+undifined
+symbole
+Object
+ */
---
+++ Basics/Variable.js
@@ -0,0 +1,10 @@
+let Accountno = 12345678;
+var UserName = "Sanket Yadav"
+const Ifsc = "Ubin05444"
+
+Agreement="confirmed"
+console.log(Accountno)
+
+// Prefer not to use var
+
+console.table([Accountno,UserName,Ifsc,Agreement]);
