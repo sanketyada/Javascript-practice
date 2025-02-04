00ed09bdd7fd9961be7550e0c2d626fcc4280526
Sanket Yadav <160760290+sanketyada@users.noreply.github.com>
2025-02-04 15:17:33 +0000

    Datatype Summery

---
+++ Basics/DatatypeSummery.js
@@ -0,0 +1,30 @@
+// There can be to types of Datatype
+// Primitive Datatype
+/* 
+String
+Number
+Boolean
+null
+undefined
+symbole
+BigInt
+ */
+
+// NonPrimitive Datatype
+/*Array 
+  Object
+  Function
+ */
+
+let arr=['Sanket',1,98.3];
+
+let myobj={
+    name:"Anket Yadav",
+    age:12,
+    Percentage:98.43
+}
+
+let myFunction=  function(){
+    console.log("Jai Jai Shree Ram");
+}
+console.log(typeof myFunction)
